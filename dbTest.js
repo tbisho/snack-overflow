29ab8c30c9098cdbde2a48414552aff83330aa60
tbisho <tess.bishop8@gmail.com>
2020-10-25 16:37:38 -0600

    updating local branch

--- dbTest.js
+++ dbTest.js
@@ -17,10 +17,8 @@ db.user.findOrCreate({
     email: "heythere@aol.com"
   }
 }).then(function([returnedUser, created]) {
-  // Second step: findOrCreate a sback to associate with Susan
+  // Second step: findOrCreate a snack to associate with Susan
   db.snack.findOrCreate({
-    // be mindful of casing and spelling because SQL is picky! 
-    // => "Stinky bear" !== "stinky bear"
     where: { name: "Doritas", description: "Wasabi" }
   }).then(function([returnedSnack, created]) {
     // Last Step: associating the user to the snack - addSnack()
