cf48ee1a606cf41651cf921123173de329231d87
mgcarbonell <mgcarbonell@gmail.com>
2020-10-26 15:41:26 -0700

    Working on delete functionality

--- routes/delete.js
+++ routes/delete.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const methodOverride = require('method-override')
-const isLoggedIn = require('./middleware/isLoggedIn');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 router.use(methodOverride('_method'))
 
 router.delete('/profile', isLoggedIn, (res, req) => {
   //Need to verify a way to ensure that the SNACK is getting deleted
   // maybe req.body.snack.id or req.body.snack.name
-  const deleteSnack = req.body.name;
-  console.log(deleteSnack)
-  db.snack.destroy({
-    where: {name: deleteSnack}
-  }).then( () => {
-    res.redirect('/profile');
-  });
+  // const deleteSnack = req.body.name;
+  // console.log(deleteSnack)
+  // db.snack.destroy({
+  //   where: {name: deleteSnack}
+  // let foundUser = db.user.findOrCreate
+  // }).then( () => {
+  //   res.redirect('/profile');
+  // });
 });
 
 module.exports = router;
