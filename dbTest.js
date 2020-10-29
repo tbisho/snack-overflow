e11e7e002a41ac2aad506215b4622f20c31ad289
mgcarbonell <mgcarbonell@gmail.com>
2020-10-29 09:45:08 -0700

    Third cleaning, removing more comments, extraneous files

--- dbTest.js
+++ dbTest.js
@@ -1,14 +1,6 @@
 const db = require('./models')
 const methodOverride = require('method-override')
 
-// db.user.findOne().then(function(user){
-//   user.createCommentReal({
-//     content: "This is the best snack ever"
-//   }).then(function(comment){
-//     console.log(commentReal.content)
-//   })
-// })
-
 db.comment.create({
   userId: 1,
   content: "This is the best snack"
